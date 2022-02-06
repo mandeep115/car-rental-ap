dff4a6636b84ca5ad8441ddce69825bf30ac6aa6
mandeep115 <mandeepab51@gmail.com>
2022-02-07 03:35:53 +0530

    changed api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const API = axios.create({
-  baseURL: "http://localhost:1000/",
+  baseURL: "http://ec2-52-66-53-59.ap-south-1.compute.amazonaws.com/",
   headers: {
     "Content-Type": "application/json",
   },
