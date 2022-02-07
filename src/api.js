e32e92b5f49823e1e8d0b5954a3164d81c1eec1c
mandeep115 <mandeepab51@gmail.com>
2022-02-07 05:52:38 +0530

    fixed url

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const API = axios.create({
-  // baseURL: "http://ec2-52-66-53-59.ap-south-1.compute.amazonaws.com/",
-  baseURL: "http://localhost:1000/",
+  baseURL: "http://ec2-52-66-53-59.ap-south-1.compute.amazonaws.com/",
+  // baseURL: "http://localhost:1000/",
   headers: {
     "Content-Type": "application/json",
   },
